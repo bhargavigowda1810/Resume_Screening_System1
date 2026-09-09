34d11af104580159fa9b9d133e920cb7c10dadc6
RAKSHA <rakshaumashankar.123@gmail.com>
2026-09-09 16:47:26 +0530

    feat: add Education entity

---
+++ backend/src/main/java/resume_screening_backend/entity/Education.java
@@ -0,0 +1,101 @@
+package resume_screening_backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "educations")
+public class Education {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "education_id")
+    private Long educationId;
+
+    @Column(name = "resume_id", nullable = false)
+    private Long resumeId;
+
+    @Column(name = "degree", length = 200)
+    private String degree;
+
+    @Column(name = "institution", length = 255)
+    private String institution;
+
+    @Column(name = "field_of_study", length = 255)
+    private String fieldOfStudy;
+
+    @Column(name = "start_year")
+    private Integer startYear;
+
+    @Column(name = "end_year")
+    private Integer endYear;
+
+    @Column(name = "grade", length = 50)
+    private String grade;
+
+    public Education() {
+    }
+
+    public Long getEducationId() {
+        return educationId;
+    }
+
+    public void setEducationId(Long educationId) {
+        this.educationId = educationId;
+    }
+
+    public Long getResumeId() {
+        return resumeId;
+    }
+
+    public void setResumeId(Long resumeId) {
+        this.resumeId = resumeId;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public String getFieldOfStudy() {
+        return fieldOfStudy;
+    }
+
+    public void setFieldOfStudy(String fieldOfStudy) {
+        this.fieldOfStudy = fieldOfStudy;
+    }
+
+    public Integer getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(Integer startYear) {
+        this.startYear = startYear;
+    }
+
+    public Integer getEndYear() {
+        return endYear;
+    }
+
+    public void setEndYear(Integer endYear) {
+        this.endYear = endYear;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
